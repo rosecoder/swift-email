99030a4c9b05cfee2030e4b034beb2a2d6dc28d0
Gustaf Rosenblad <gustaf@ccode.se>
2024-05-01 09:25:35 +0200

    fix: Global styles resolved with incorrect environment variables

--- Sources/SwiftEmail/Modifiers/View+GlobalStyle.swift
+++ Sources/SwiftEmail/Modifiers/View+GlobalStyle.swift
@@ -2,23 +2,15 @@ extension View {
 
     public func unsafeGlobalStyle(
         _ key: String,
-        _ value: String,
+        _ value: any ShapeStyle,
         selector: CSSSelector
     ) -> some View {
-        unsafeGlobalStyle(key, { _ in value }, selector: selector)
+        GlobalStyleOverride(key: key, value: AnyShapeStyle(value), selector: selector, content: self)
     }
 
     public func unsafeGlobalStyle(
         _ key: String,
-        _ value: any ShapeStyle,
-        selector: CSSSelector
-    ) -> some View {
-        unsafeGlobalStyle(key, { await value.renderCSSValue(environmentValues: $0) }, selector: selector)
-    }
-
-    private func unsafeGlobalStyle(
-        _ key: String,
-        _ value: @escaping (EnvironmentValues) async -> String,
+        _ value: CSSValue,
         selector: CSSSelector
     ) -> some View {
         GlobalStyleOverride(key: key, value: value, selector: selector, content: self)
@@ -28,7 +20,7 @@ extension View {
 private struct GlobalStyleOverride<Content: View>: View {
 
     let key: String
-    let value: (EnvironmentValues) async -> String
+    let value: CSSValue
     let selector: CSSSelector
     let content: Content
 }
@@ -38,7 +30,7 @@ extension GlobalStyleOverride: PrimitiveView {
     func renderRootHTML(options: HTMLRenderOptions, context: HTMLRenderContext) async -> String {
         await context.globalStyle.insert(
             key: key,
-            value: await value(context.environmentValues),
+            value: value,
             selector: selector
         )
         return await content.renderHTML(options: options, context: context)
