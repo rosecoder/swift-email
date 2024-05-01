fab0a881be1fbebfe5865ea8d20760038029042b
Gustaf Rosenblad <gustaf@ccode.se>
2024-05-01 09:45:37 +0200

    feat: Set default link color in dark mode when using NavigationLink

--- Sources/SwiftEmail/Views/NavigationLink.swift
+++ Sources/SwiftEmail/Views/NavigationLink.swift
@@ -2,6 +2,7 @@ public struct NavigationLink<Label>: View where Label: View {
 
     @Environment(\.buttonStyle) private var buttonStyle
     @Environment(\.foregroundStyle) private var foregroundStyle
+    @Environment(\.globalStyle) private var globalStyle
 
     public enum Destination {
         case url(String)
@@ -21,21 +22,26 @@ public struct NavigationLink<Label>: View where Label: View {
     }
 
     public var body: some View {
-        let styles = {
-            var styles = Styles()
-            if !(buttonStyle.content is LinkButtonStyle) {
-                styles["text-decoration"] = "none" // Underline can be added through .underline() in the button style itself
+        get async {
+            let styles = await {
+                var styles = Styles()
+                if !(buttonStyle.content is LinkButtonStyle) {
+                    styles["text-decoration"] = "none" // Underline can be added through .underline() in the button style itself
+                }
+                styles["color"] = foregroundStyle
+                if (foregroundStyle.content as? Color)?.hasAlternative != false {
+                    await globalStyle.insert(key: "color", value: foregroundStyle, selector: .element("a", colorScheme: .dark))
+                }
+                return styles
+            }()
+            UnsafeNode(tag: "a", attributes: [
+                "href": destination,
+                "style": styles
+            ]) {
+                buttonStyle.makeBody(configuration: AnyButtonStyle.Configuration(
+                    label: AnyButtonStyle.Configuration.Label(label)
+                ))
             }
-            styles["color"] = foregroundStyle
-            return styles
-        }()
-        UnsafeNode(tag: "a", attributes: [
-            "href": destination,
-            "style": styles
-        ]) {
-            buttonStyle.makeBody(configuration: AnyButtonStyle.Configuration(
-                label: AnyButtonStyle.Configuration.Label(label)
-            ))
         }
     }
 }
