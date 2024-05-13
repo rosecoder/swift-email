17b64f488d4296d51f479cbc004413859c809eeb
Gustaf Rosenblad <gustaf@ccode.se>
2024-05-13 07:03:22 +0200

    feat: Added support for custom divider color

--- Sources/SwiftEmail/Views/Divider.swift
+++ Sources/SwiftEmail/Views/Divider.swift
@@ -1,8 +1,48 @@
 public struct Divider: View {
 
+    var style: AnyShapeStyle = AnyShapeStyle(Color("D6D3D4"))
+    var className: ClassName?
+
     public init() {}
 
-    public var body: some View {
-        UnsafeNode("<hr/>")
+    public var body: some View { noBody }
+}
+
+extension Divider {
+
+    public func overlay<Style: ShapeStyle>(
+        _ style: Style,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> Divider {
+        let className = ClassName(uniqueAt: file, line: line)
+        var view = self
+        view.style = AnyShapeStyle(style)
+        view.className = className
+        return view
+    }
+}
+
+extension Divider: PrimitiveView {
+
+    func _render(options: RenderOptions, context: RenderContext) async -> RenderResult {
+        let node = if let className {
+            AnyView(
+                await UnsafeNode(tag: "hr", attributes: [
+                    "style": "border:0;border-top:1px solid " + style.renderCSSValue(environmentValues: context.environmentValues),
+                    "class": className.renderCSS(options: options),
+                ])
+                .unsafeGlobalStyle("border-top-color", style as any ShapeStyle, selector: .className(className, colorScheme: .dark))
+            )
+        } else {
+            AnyView(await UnsafeNode(tag: "hr", attributes: [
+                "style": "border:0;border-top:1px solid " + style.renderCSSValue(environmentValues: context.environmentValues),
+            ]))
+        }
+
+        return .init(
+            html: await node.render(options: options, context: context).html,
+            text: "\n"
+        )
     }
 }
--- Tests/SwiftEmailTests/DividerTests.swift
+++ Tests/SwiftEmailTests/DividerTests.swift
@@ -1,11 +1,22 @@
 import XCTest
+import SnapshotTesting
 import SwiftEmail
+import SwiftEmailTesting
 
 final class DividerTests: XCTestCase {
 
-    func testRenderHTML() async throws {
+    func testRender() async throws {
         let render = await Divider().render()
-        XCTAssertEqual(render.html, "<hr/>")
-        XCTAssertEqual(render.text, "")
+        XCTAssertEqual(render.html, "<hr style=\"border:0;border-top:1px solid #D6D3D4\"/>")
+        XCTAssertEqual(render.text, "\n")
+    }
+
+    func testRenderWithColor() {
+        let email = Email {
+            Divider()
+                .overlay(Color.text)
+        }
+        assertSnapshot(of: email, as: .html)
+        assertSnapshot(of: email, as: .text)
     }
 }
