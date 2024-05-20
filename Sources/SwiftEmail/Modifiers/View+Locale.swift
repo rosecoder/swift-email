d83d87be395850489dbe3cd2ee8f4391d1593d66
Gustaf Rosenblad <gustaf@ccode.se>
2024-05-20 07:17:20 +0200

    feat: Added Locale-environment variable

---
+++ Sources/SwiftEmail/Modifiers/View+Locale.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+private struct LocaleKey: EnvironmentKey {
+
+    static var defaultValue: Locale {
+        .current
+    }
+}
+
+extension EnvironmentValues {
+
+    public var locale: Locale {
+        get { self[LocaleKey.self] }
+        set { self[LocaleKey.self] = newValue }
+    }
+}
