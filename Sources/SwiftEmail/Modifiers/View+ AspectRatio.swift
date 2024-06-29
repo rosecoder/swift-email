5e77918ee32520309f57b145adfbf7b755739f55
Gustaf Rosenblad <gustaf@ccode.se>
2024-06-29 07:02:58 +0200

    feat: Added content mode environment variable

---
+++ Sources/SwiftEmail/Modifiers/View+ AspectRatio.swift
@@ -0,0 +1,32 @@
+private struct ContentModeKey: EnvironmentKey {
+
+    static var defaultValue: ContentMode?
+}
+
+extension EnvironmentValues {
+
+    public var contentMode: ContentMode? {
+        get { self[ContentModeKey.self] }
+        set { self[ContentModeKey.self] = newValue }
+    }
+}
+
+public enum ContentMode {
+    case fill
+    case fit
+}
+
+extension View {
+
+    public func aspectRatio(contentMode: ContentMode) -> some View {
+        environment(\.contentMode, contentMode)
+    }
+
+    public func scaledToFill() -> some View {
+        aspectRatio(contentMode: .fill)
+    }
+
+    public func scaledToFit() -> some View {
+        aspectRatio(contentMode: .fit)
+    }
+}
